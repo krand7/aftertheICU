d7663232e768a56632e17ac19283149aedf65bd9
Kyle Rand <krand7@gmail.com>
2015-09-22 23:43:52 -0400

    Updated styling of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ActiveRecord::Base
   # Model Relationships
   belongs_to :topic
   belongs_to :user
+
+  # Model Methods
+  def is_first_post?
+    self.topic.posts.first == self
+  end
 end
